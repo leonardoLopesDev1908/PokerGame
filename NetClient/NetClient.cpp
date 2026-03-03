c778dc5245aefd3960a3d47e4a20437438bd775e
leonardoLopesDev1908 <leonardosilvalls1908@gmail.com>
2026-03-03 20:10:39 -0300

    client.cpp input

--- NetClient/NetClient.cpp
+++ NetClient/NetClient.cpp
@@ -1,11 +1,37 @@
 ﻿#include <iostream>
 #include <olc_net_client.h>
 
-class Client : public net::tcp::client<net::MessageType>
+
+enum class ClientMessages
 {
+	Ping, 
+	Connect,
+	Fold,
+	Call,
+	Raise,
+	Transaction,
+	End
+}
+
+class Client : public net::tcp::client<PokerMessages>
+{
+public:
+
+	enum class TransactionType
+	{
+		Lost,
+		Win
+	}
+
+	
 	void ping()
 	{
-	
+		net::tcp::message<PokerMessages> msg;
+		msg << "ECHO\n";
+
+		msg.header.id = ClientMessages::Ping;
+
+		c.send(msg);
 	}
 
 	void raise()
@@ -21,7 +47,35 @@ class Client : public net::tcp::client<net::MessageType>
 	void fold()
 	{
 	}
+
+	void end()
+	{
+		std::cout << "Requesting disconnection from server...\n";
+		c.disconnect();
+	}
+
+	bool update_money(long int value, TransactionType type)
+	{
+		if(type == TransactionType::Lost)
+		{
+			m_money -= value;
+		}
+		else if(type == TransactionType::Win)
+		{
+			m_money += value;
+		}
+		else{
+			std::cerr << "Invalid type of transaction: " << type << '\n';
+			return false;
+		}
+		
+		return true;
+	}
+
+private:
+	long long int m_money;
 };
+
 
 int main()
 {
@@ -30,16 +84,29 @@ int main()
 	std::this_thread::sleep_for(std::chrono::seconds(2));
 
 	char text[512];
-	while (true)
+
+	bool bQuit = false;
+	while (!bQuit)
 	{
-		std::cout << "Message: ";
-		std::cin >> text;
 
-		net::tcp::message<net::MessageType> msg;
+		if (GetAsyncKeyState('1') && 0x8000) c.ping();
+		else if(GetAsyncKeyState('2') && 0x8000) 
+		{
+			c.end();
+			bQuit = true;
+			break;
+		}
+		else if(GetAsyncKeyState('3') && 0x8000) c.call();
 
-		msg << text;
-		c.send(msg);
-		std::cout << '\n';
+		if(!c.incoming().empty())
+		{
+			auto msg = c.incoming().pop_front();
+			switch(msg.header.id)
+			{
+				
+
+			}
+		}
 	}
 
 	return 0;
